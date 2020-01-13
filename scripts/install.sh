f86733b047ed635a8ad33156bfa7c810fed9d9a8
Jacek Wysocki <jacek.wysocki@gmail.com>
2020-01-13 14:17:48 +0100

    install script update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,7 @@
 repo_url="https://github.com/exu/sailr"
 release_tag=master
 script_file="https://raw.githubusercontent.com/exu/sailr/$release_tag/sailr.sh"
+json_file="https://raw.githubusercontent.com/exu/sailr/$release_tag/sailr.json"
 
 function init {
     if [ ! -z $1 ]; then
@@ -11,8 +12,16 @@ function init {
 
     destination="${PWD}/.git/hooks"
 
-    download_status=$(curl $script_file -o "${destination}/commit-msg")
+    curl $script_file -o "${destination}/commit-msg"
     chmod u+x "${destination}/commit-msg"
+
+    if [ ! -d "$HOME/.sailr" ] 
+    then
+        mkdir $HOME/.sailr/
+        curl $json_file -o "$HOME/.sailr/sailr.json"
+    fi
+
+    echo 'export SAILR_CONFIG=$HOME/.sailr/sailr.json' >> ~/.zshrc
 
     echo -e "\nInstalled Sailr as \e[33mcommit-msg\033[0m hook in \e[32m$destination\033[0m."
     echo "For usage see https://github.com/exu/sailr#usage"
