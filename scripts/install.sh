1514e6d33b27b234d62895d638fbcf1165b10baf
Jacek Wysocki <jacek.wysocki@gmail.com>
2020-01-13 13:46:38 +0100

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-repo_url="https://github.com/craicoverflow/sailr"
+repo_url="https://github.com/exu/sailr"
 release_tag=master
-script_file="https://raw.githubusercontent.com/craicoverflow/sailr/$release_tag/sailr.sh"
+script_file="https://raw.githubusercontent.com/exu/sailr/$release_tag/sailr.sh"
 
 function init {
     if [ ! -z $1 ]; then
@@ -15,7 +15,7 @@ function init {
     chmod u+x "${destination}/commit-msg"
 
     echo -e "\nInstalled Sailr as \e[33mcommit-msg\033[0m hook in \e[32m$destination\033[0m."
-    echo "For usage see https://github.com/craicoverflow/sailr#usage"
+    echo "For usage see https://github.com/exu/sailr#usage"
 }
 
 init $1
